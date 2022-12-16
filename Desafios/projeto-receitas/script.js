ffddc255e87e9673533a15d9e89e3aa760f0e38f
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2022-12-16 17:01:51 -0300

    Atualizei icon-mobile

--- Desafios/projeto-receitas/script.js
+++ Desafios/projeto-receitas/script.js
@@ -1,13 +1,14 @@
 function menushow(){
     let menumobile =document.querySelector('.mobile-menu-nav')
     var nav = document.querySelector('.nav-bar')
-    let img = document.querySelector('icon-mobile')
+    let img = document.querySelector('#icon-mobile')
     if(menumobile.classList.contains('open')){
         menumobile.classList.remove('open');
         nav.style.height='65px'
-
+        img.src="imagens/icon-mobile-white.png"
     }else{
         menumobile.classList.add('open')
         nav.style.height='285px'
+        img.src="imagens/icon-mobile-x.png"
     }
 }
