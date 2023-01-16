eb886f5e01ad718d96a9973d630f011becfbeb2b
Emerson Rodrigues <119687303+RodriguesEmerson@users.noreply.github.com>
2023-01-16 12:04:08 -0300

    Criei um mais um Carrosel - Slide
    Criei um carrossel quase 100% dinâmico.

--- estudos-extra/carrossel-slide/slide2/script.js
+++ estudos-extra/carrossel-slide/slide2/script.js
@@ -1,21 +1,84 @@
 let prev =document.querySelector('.anterior')
 let prox = document.querySelector('.posterior')
 
+let imgONE = document.querySelector('#img-ti-1')
+let imgTWO = document.querySelector('#img-ti-2')
+
 let troca_imagem = document.querySelector('.troca-imagem')
 
+var numIMG = 1
+function carregaImagens(){
+    imgONE.setAttribute('src', `imagens/img-${numIMG}.jpg`)
+    imgTWO.setAttribute('src', `imagens/img-${numIMG + 1}.jpg`)
+}
+
 prev.addEventListener('click', function(){
-    troca_imagem.style.margin ='0 0 0 0'
+//imagens/img-2.jpg
+    
+    let srcIMG_ONE = imgONE.getAttribute('src')
+    
+    let posi = troca_imagem.offsetLeft
+    if(posi === 0){
+        numIMG--
+        if(numIMG<1){
+            numIMG = 5
+        }
+
+        imgONE.setAttribute('src', `imagens/img-${numIMG}.jpg`)
+
+        imgTWO.setAttribute('src', srcIMG_ONE)
+        
+
+        //Adiciona o efeito de transição
+        if(troca_imagem.classList.contains('esquerda')){
+            troca_imagem.classList.remove('esquerda', 'direita', 'direita2')
+            troca_imagem.classList.add('esquerda2')
+        }else{
+            troca_imagem.classList.add('esquerda')
+            troca_imagem.classList.remove('esquerda2', 'direita', 'direita2')
+        }
+    }else{
+        numIMG--
+        if(numIMG<1){
+            numIMG = 5
+        }
+        troca_imagem.classList.remove('direita', 'direita2')
+        troca_imagem.classList.add('esquerda')
+    }
+
+ 
 })
 prox.addEventListener('click', function(){
 
-    posi = troca_imagem.offsetLeft
-    console.log(posi)
-    if(posi !== 0){
-        troca_imagem.style.margin = "0 0 0 0"
-        troca_imagem.style.transform = 'transition: 0s'
-        //troca_imagem.classList.add('esquerda')
+    let imgSRC_TWO = imgTWO.getAttribute('src')
+
+    let posi = troca_imagem.offsetLeft
+    if(posi != 0){
+
+        numIMG++
+        if(numIMG>5){
+            numIMG = 1
+        }
+
+        imgONE.setAttribute('src', imgSRC_TWO)
+        imgTWO.setAttribute('src', `imagens/img-${numIMG}.jpg`)
+
+        troca_imagem.style.margin = '0 0 0 0'
+         if(troca_imagem.classList.contains('direita')){
+           troca_imagem.classList.remove('direita', 'esquerda', 'esquerda2')
+           troca_imagem.classList.add('direita2')
+         }else{
+        troca_imagem.classList.add('direita')
+        troca_imagem.classList.remove('direita2', 'esquerda', 'esquerda2')
+         }
+        
     }else{
-        troca_imagem.style.margin = "0 0 0 -100%"
+        numIMG++
+        if(numIMG>5){
+            numIMG = 1
+        }
+        troca_imagem.classList.remove('esquerda', 'esquerda2')
+        troca_imagem.classList.add('direita')
         
     }
 })
